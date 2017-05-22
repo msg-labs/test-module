d2eaf9d46692943c4c2ec0ede13f0d9bb034ad0c
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2017-05-22 18:26:31 +0100

    Use a helper for assertions

--- index.tests.js
+++ index.tests.js
@@ -4,26 +4,29 @@ const assert = require( 'assert' );
 const rxjs = require( 'rxjs' );
 
 const tick = ( message = 'Passed' ) => console.log( `    ✓ ${message}` );
+const ssrt = ( value, failure, success ) => assert( value, failure ) || tick( success );
 
-assert( msg.init,
-    'It should contain an init property' ) || tick( 'Has init' );
+ssrt( msg.init, 'It should contain an init property', 'Has init' );
 
-assert.strictEqual( typeof msg.init, 'function',
-    'Init should be a function' ) || tick( 'Init is a function' );
+ssrt( typeof msg.init === 'function',
+    'Init should be a function',
+    'Init is a function' );
 
-assert( msg.init() instanceof rxjs.Observable,
-    'Init should return an observable' ) || tick( 'Init returns an observable' );
+ssrt( msg.init() instanceof rxjs.Observable,
+    'Init should return an observable',
+    'Init returns an observable' );
 
 
-const subscribeNext = () => assert( true,
-    'It should call the next function' ) || tick( 'Calls the next method' );
+const subscribeNext = () => ssrt( true,
+    'It should call the next function',
+    'Calls the next method' );
 
-const subscribeError = () => assert( false,
+const subscribeError = () => ssrt( false,
     'It should not call the error function' );
 
-const subscribeComplete = () => assert( true,
-    'It should call the complete function' ) ||
-    tick( 'Calls the complete method' );
+const subscribeComplete = () => ssrt( true,
+    'It should call the complete function',
+    'Calls the complete method' );
 
 msg.init()
     .subscribe( {
@@ -38,23 +41,26 @@ let nextTimes = 0;
 msg.init()
     .subscribe( {
         next: () => nextTimes++,
-        complete: () => assert.strictEqual( nextTimes, 1,
-            'It should call next once and complete' ) ||
-        tick( 'It calls next one time and then completes' )
+        complete: () => ssrt( nextTimes === 1,
+            'It should call next once and complete',
+            'It calls next one time and then completes' )
     } );
 
-const nextReturnsString = message => assert( typeof message === 'string',
-    'It should return an string' ) || tick( 'Returns an string' );
+const nextReturnsString = message => ssrt( typeof message === 'string',
+    'It should return an string',
+    'Returns an string' );
 
 msg.init()
     .subscribe( {
         next: nextReturnsString
     } );
 
-const nextWithDefaultMessage = message =>
-    assert.strictEqual( message, 'Unknow user, Welcome to msg labs!',
-        'It should return a default message if no name is provided' ) ||
-    tick( 'Returns the default message when no name is provided' );
+
+const unknowMessage = 'Unknow user, Welcome to msg labs!';
+
+const nextWithDefaultMessage = message => ssrt( message === unknowMessage,
+    'It should return a default message if no name is provided',
+    'Returns the default message when no name is provided' );
 
 msg.init()
     .subscribe( {
@@ -62,14 +68,15 @@ msg.init()
     } );
 
 
-const nameParam = 'Test';
-const assertNextWithParam = message =>
-    assert.strictEqual( message, `Test, Welcome to msg labs!`,
-        'It should customise the welcome message if a name is provided' ) ||
-    tick( 'Customises the message if a parameter is provided' );
+const userMessage = 'Test, Welcome to msg labs!';
+const userName = 'Test';
+
+const assertNextWithParam = message => ssrt( message === userMessage,
+    'It should customise the welcome message if a name is provided',
+    'Customises the message if a parameter is provided' );
 
 
-msg.init( nameParam )
+msg.init( userName )
     .subscribe( {
         next: assertNextWithParam
     } );
